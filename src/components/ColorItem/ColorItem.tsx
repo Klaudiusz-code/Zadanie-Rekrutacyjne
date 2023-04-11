dea1046c8a727244d36e585ac47d27befb47e4f0
Klaudiusz <muminowy123@gmail.com>
2023-04-11 08:40:49 +0200

    Added functionality to remove colors from the list in ColorItem

--- src/components/ColorItem/ColorItem.tsx
+++ src/components/ColorItem/ColorItem.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
-const ColorItem = () => {
+import './ColorItem.scss';
+
+interface Props {
+    color: string;
+    backgroundColor: string;
+}
+
+const ColorItem = ({ color }: Props) => {
+
+    const handleRemoveClick = () => {
+        if (window.confirm(`Czy na pewno chcesz usunąć kolor ${color}?`)) {
+            const colorsFromLocalStorage = JSON.parse(localStorage.getItem('colors') || '[]');
+            const newColors = colorsFromLocalStorage.filter((c: string) => c !== color);
+            localStorage.setItem('colors', JSON.stringify(newColors));
+            window.location.reload();
+        }
+    }
+
     return (
-        <div>
-            <h1>ColorItem</h1>
+        <div className="color-item">
+            <div className="color-item__swatch" style={{ backgroundColor: color }}></div>
+            <div className="color-item__name" style={{ backgroundColor: color }}>{color}</div>
+            <button className="color-item__remove-button" onClick={handleRemoveClick}>Usuń</button>
         </div>
     );
 };

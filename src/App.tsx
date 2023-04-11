70bd97dee4f0b83aef08dd14b9dd6332989e825e
Klaudiusz <muminowy123@gmail.com>
2023-04-11 08:43:29 +0200

    Adding AddColorForm and ColorList components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import AddColorForm from "./components/AddColorForm/AddColorForm";
+import ColorList from "./components/ColorList/ColorList";
 
 function App() {
   return (
     <div className="App">
-        <h1>App</h1>
+        <AddColorForm/>
+        <ColorList/>
     </div>
   );
 }

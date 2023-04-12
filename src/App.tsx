74d7eeb81b1147756e704e767e82fc364c49b752
Klaudiusz <muminowy123@gmail.com>
2023-04-12 09:24:03 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import AddColorForm from "./components/AddColorForm/AddColorForm";
 import ColorList from "./components/ColorList/ColorList";
 

45bac6b690096da2add27bacf4e11549829cfffa
Klaudiusz <muminowy123@gmail.com>
2023-04-12 16:17:10 +0200

    Added class modal-container

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -11,21 +11,23 @@ interface Modal {
 
 const ModalChoice: React.FC<Modal> = ({ text, onConfirm, onCancel }) => {
     return (
-        <div className="modal">
-            <div className="modal__content">
-                <div className="modal__header">
-                    <h3 className="modal__title">{text}</h3>
-                </div>
+        <div className="modal-container">
+            <div className="modal">
+                <div className="modal__content">
+                    <div className="modal__header">
+                        <h3 className="modal__title">{text}</h3>
+                    </div>
 
-                <div className="modal__footer">
-                    <button className="btn btn__success" onClick={onConfirm}>
-                      <span className="modal__icon"><MdDone size={17} /></span>
-                        Yes
-                    </button>
-                    <button className="btn btn__danger" onClick={onCancel}>
-                        <span className="modal__icon"><VscChromeClose size={17} /></span>
-                        No
-                    </button>
+                    <div className="modal__footer">
+                        <button className="btn btn__success" onClick={onConfirm}>
+                          <span className="modal__icon"><MdDone size={17} /></span>
+                            Yes
+                        </button>
+                        <button className="btn btn__danger" onClick={onCancel}>
+                            <span className="modal__icon"><VscChromeClose size={17} /></span>
+                            No
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
